b3a8b42275df394da07c9c7cce12cf1bc5a9a557
Bartek Drozdz <bartek@everyday3d.com>
2013-09-10 16:16:22 -0700

    Added other server support

--- bin/01/socketio.js
+++ bin/01/socketio.js
@@ -1,9 +1,33 @@
-var io = require('socket.io').listen(8123);
+var io = require('socket.io')
+var net = require('net');
+
+var nst;
+var led = "0";
 
 io.sockets.on('connection', function (socket) {
 
     socket.on('interact', function (data) {
         socket.emit('interact', data.message);
         socket.broadcast.emit('interact', data.message);
+
+        if(led == "0") led = "1";
+        else led = "0";
+
+        if(nst) nst.write(led);
     });
 });
+
+ns = net.createServer(function (socket) {
+
+    nst = socket;
+
+    console.log("new client");
+
+    socket.on('end', function() {
+        nst = null;
+    })
+
+});
+
+io.listen(8123);
+ns.listen(1337);
