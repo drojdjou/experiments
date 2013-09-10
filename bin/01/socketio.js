0acd23def9ebaf34c92a421d2634b2194a55a751
Bartek Drozdz <bartek@everyday3d.com>
2013-09-10 13:15:56 -0700

    Socket.io intergation

--- bin/01/socketio.js
+++ bin/01/socketio.js
@@ -1,6 +1,11 @@
 var io = require('socket.io').listen(8080);
 
+console.log("Server started on 8080");
+
 io.sockets.on('connection', function (socket) {
+
+    console.log("Incoming connection");
+
     socket.emit('news', { hello: 'world' });
     socket.on('my other event', function (data) {
         console.log(data);
