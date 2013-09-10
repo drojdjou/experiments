b11243fee7766b492e9742e8a34e11aa90f069d6
Bartek Drozdz <bartek@everyday3d.com>
2013-09-10 13:33:26 -0700

    Updated socket server

--- bin/01/socketio.js
+++ bin/01/socketio.js
@@ -2,8 +2,7 @@ var io = require('socket.io').listen(8123);
 
 io.sockets.on('connection', function (socket) {
 
-    socket.on('touch', function (data) {
-        console.log("- touch: " + data.message);
-        socket.emit(data.message);
+    socket.on('interact', function (data) {
+        socket.emit('interact', data.message);
     });
 });
