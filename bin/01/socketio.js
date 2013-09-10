dd15c256960dc194f4c25ddcd9a67be606a224c5
Bartek Drozdz <bartek@everyday3d.com>
2013-09-10 13:26:36 -0700

    Adding communication

--- bin/01/socketio.js
+++ bin/01/socketio.js
@@ -1,13 +1,9 @@
 var io = require('socket.io').listen(8123);
 
-console.log("Server started on 8123");
-
 io.sockets.on('connection', function (socket) {
 
-    console.log("Incoming connection");
-
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
+    socket.on('touch', function (data) {
+        console.log("- touch: " + data);
+        socket.emit(data);
     });
 });
