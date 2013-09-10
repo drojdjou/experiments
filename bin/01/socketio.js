0cd3430ab25b8c12896305e180517e03804264f4
Ubuntu <ubuntu@ip-172-31-20-212.us-west-2.compute.internal>
2013-09-10 20:10:15 +0000

    Adding socket.io lib files

--- bin/01/socketio.js
+++ bin/01/socketio.js
@@ -1,4 +1,4 @@
-var io = require('socket.io').listen(80);
+var io = require('socket.io').listen(8080);
 
 io.sockets.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
