5ee7bea3b7962d3a459baa7dc08c8ce1dacb0f18
Bartek Drozdz <bartek@everyday3d.com>
2013-10-10 15:42:19 -0700

    Server fix

--- bin/10/server10.js
+++ bin/10/server10.js
@@ -19,16 +19,16 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('log', function (data) {
     	//console.log("Log", data);
-    	if(logReader) logReader.emit("log", data);
+    	for(var i = 0; i < logReaders.length; i++) logReaders[i].emit("log", data);
     });
 
     socket.on('warn', function (data) {
     	//console.log("Warn", data);
-    	if(logReader) logReader.emit("warn", data);
+    	for(var i = 0; i < logReaders.length; i++) logReaders[i].emit("warn", data);
     });
 
     socket.on('error', function (data) {
     	//console.log("Error", data);
-    	if(logReader) logReader.emit("error", data);
+    	for(var i = 0; i < logReaders.length; i++) logReaders[i].emit("error", data);
     });
 });
